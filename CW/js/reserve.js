3008f686036fb6461da410ee628dcbc9aa2c3bbc
Greatness10 <157973624+Greatness10@users.noreply.github.com>
2024-03-25 11:58:57 +0000

    opening

--- CW/js/reserve.js
+++ CW/js/reserve.js
@@ -1,11 +1,5 @@
 document.getElementById("booking-form").addEventListener("submit", function(event) {
-    // Prevent default form submission
+    
     event.preventDefault();
-    // Handle form submission here, e.g., sending data to a server
-    // You can use AJAX or fetch API for this purpose
-    // Example:
-    // const formData = new FormData(event.target);
-    // const jsonData = Object.fromEntries(formData.entries());
-    // console.log(jsonData);
-    // Then you can send jsonData to the server
+    
 });
