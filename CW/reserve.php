ccbc020eaca1fa70b8c818bbd8af7582acadaa49
Greatness10 <157973624+Greatness10@users.noreply.github.com>
2024-04-15 10:09:36 +0100

    upd

--- CW/reserve.php
+++ CW/reserve.php
@@ -12,7 +12,7 @@
         <nav>
             <ul>
                 <li><a href="#services">Services</a></li>
-                <li><a href="#about">About Us</a></li>
+                <li><a href="#home.php">About Us</a></li>
                 <li><a href="#gallery">Gallery</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
@@ -67,3 +67,39 @@
     <script src="js/login.js"></script>
 </body>
 </html>
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "the mane event";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+    $service_id = $_POST['service_id'];
+    $customer_name= $_POST['customer_name'];
+    $stylist = $_POST['stylist'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $booking_date = $_POST['booking_date'];
+
+    
+    $sql = "INSERT INTO bookings (id, service_id, customer_name, stylist, name, email, phone, booking_dates) VALUES ('$id', '$service_id', '$customer_name', '$stylist', '$email', '$phone', '$booking_date')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('Booking successful!');</script>";
+    } else {
+        echo "<script>alert('Error: " . $sql . "\\n" . $conn->error . "');</script>";
+    }
+
+    $conn->close();
+}
+?>
